aad03051352dbe467f0c1f386e7cba71b48de636
yukuji <liane.jbm@gmail.com>
2023-06-28 13:18:57 -0600

    Initial Commit

---
+++ trophyRoom/trophyRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  trophyRoomApp.swift
+//  trophyRoom
+//
+//  Created by Scholar on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct trophyRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
